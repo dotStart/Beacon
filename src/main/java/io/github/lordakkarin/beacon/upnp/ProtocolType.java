4fd718d34395faa7453649d9e7b7fd169caf844b
Johannes Donath <johannesd@torchmind.com>
2016-05-23 00:41:26 +0200

    Added a protocol type representation.

---
+++ src/main/java/io/github/lordakkarin/beacon/upnp/ProtocolType.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright 2016 Johannes Donath <johannesd@torchmind.com>
+ * and other copyright owners as documented in the project's IP log.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * 	http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.github.lordakkarin.beacon.upnp;
+
+/**
+ * <strong>Protocol Type</strong>
+ *
+ * Provides a list of valid service protocol types.
+ *
+ * @author <a href="mailto:johannesd@torchmind.com">Johannes Donath</a>
+ */
+public enum ProtocolType {
+        TCP,
+        UDP
+}
