090595da263a42d5588b09ba560aa97d4d4a181f
Johannes Donath <johannesd@torchmind.com>
2019-06-16 16:11:19 +0200

    Created a specification for specialized repository loaders.

---
+++ ui/src/main/kotlin/tv/dotstart/beacon/repository/loader/RepositoryLoader.kt
@@ -0,0 +1,40 @@
+/*
+ * Copyright (C) 2019 Johannes Donath <johannesd@torchmind.com>
+ * and other copyright owners as documented in the project's IP log.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package tv.dotstart.beacon.repository.loader
+
+import java.net.URI
+import java.nio.file.Path
+
+/**
+ * Provides the logic necessary for retrieving a repository through a specific interface.
+ *
+ * @author [Johannes Donath](mailto:johannesd@torchmind.com)
+ */
+interface RepositoryLoader {
+
+  /**
+   * Identifies one or more URI schemes which are recognized and supported by this particular
+   * loader implementation.
+   */
+  val schemes: List<String>
+
+  /**
+   * Instructs this particular loader to retrieve a given repository index and store its contents at
+   * the given location.
+   */
+  operator fun invoke(uri: URI, target: Path)
+}
